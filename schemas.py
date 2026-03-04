ced7db4486923c24a27bad5d1b19b1ae8c804a6f
Gaurav Dev <gauravdev0011@gmail.com>
2026-03-04 11:48:27 -0600

    Add UserLogin schema for authentication

--- schemas.py
+++ schemas.py
@@ -1,22 +1,19 @@
+# schemas.py
 from pydantic import BaseModel
-
 
 # -------- USER --------
 class UserCreate(BaseModel):
     username: str
     password: str
 
-
 class UserLogin(BaseModel):
     username: str
     password: str
-
 
 # -------- TASK --------
 class TaskCreate(BaseModel):
     title: str
     user_id: int
-
 
 class TaskUpdate(BaseModel):
     completed: bool
