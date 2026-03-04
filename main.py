35874fcb84765dbe6642ee8faacc4de1bf2ddcef
Gaurav Dev <gauravdev0011@gmail.com>
2026-03-04 11:49:46 -0600

    Corrected main by removing unnnecssary comments

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
+# main.py
 from fastapi import FastAPI
 from database import engine
 import models
-
 from routes import users, tasks
 
 app = FastAPI(title="OmniTask API")
 
-# Create database tables
 models.Base.metadata.create_all(bind=engine)
 
-# Register routes
 app.include_router(users.router)
 app.include_router(tasks.router)
-
 
 @app.get("/")
 def root():
